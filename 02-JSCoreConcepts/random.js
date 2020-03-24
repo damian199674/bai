96a1090bdcc122da145ab03420b17e2a772e19b6
Damian Górski <damiangorski199674@gmail.com>
2020-03-24 18:55:50 +0100

    correction and finish 03

--- 02-JSCoreConcepts/random.js
+++ 02-JSCoreConcepts/random.js
@@ -1,8 +1,21 @@
-const random_tab = () => {
+/*const random_tab = () => {
     const tab = [];
     for(let i=0; i<10; i++){
         tab.push(5 + Math.floor((21 - 5) * Math.random()));
     }
     return tab;
 }; 
-console.log(random_tab());
+console.log(random_tab());*/
+
+let random_number = () => {
+   return 5 + Math.floor((21 - 5) * Math.random());
+}
+let fill_table = () =>{
+    let tab = [];
+    for(let i=0; i<10; i++){
+        tab.push(random_number());
+    }
+    console.log(tab)
+}
+fill_table()
+
